d1e5f2e142801a065c5467ecd4cd9bcab7a72269
MH <maria.higgins17@imperial.ac.uk>
2020-02-20 21:39:49 +0000

    fixed

--- Week-3/toys.py
+++ Week-3/toys.py
@@ -9,7 +9,7 @@ with functions.
 # write a function that adds 1
 # to the input and prints the result
 def inc(a):
-    a=a+2
+    a=a+1
     print(a)
 
 
@@ -17,16 +17,16 @@ def inc(a):
 # to the input and returns the result
 def inc_return(a):
     a = a+1
-    return a # hint this is incomplete - note still not right
+    return a # hint this is incomplete 
 
 
 # write a function that adds
 # the two input numbers together
 # and returns the sum
 def sum(a,b):
-    addition=a+b
-    print (addition)
-    #return a + b
+    #addition=a+b
+    #addition
+    return a + b
 
 
 # write a function that takes two
@@ -34,7 +34,9 @@ def sum(a,b):
 # sum() and then increments the sum
 # using inc_return
 def sum_inc(a, b):
-    return
+    c= sum(a,b)
+    d=inc_return(c)
+    return d
 
 
 # write a function that returns a 
@@ -54,4 +56,6 @@ def string_repeat(phrase, repeat):
     # hint: you can add strings together 
     # in order to concatenate them
     return
+
+#inc(5)
 
